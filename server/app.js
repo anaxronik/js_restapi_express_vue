c149ee0f6130f35ff7edcb1ef8079a8fbfa83bd9
Sergey Komarov <anaxronik@gmail.com>
2020-04-23 21:24:31 +0500

    add server get function

--- server/app.js
+++ server/app.js
@@ -1,5 +1,20 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 
+
+
+
+
+
+// подключение статической папки
+app.use(express.static(path.resolve(__dirname, '../', 'client')))
+
+// обработка всех входящих get запросов
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../', 'client', 'index.html'))
+})
+
+// запуск самого сервера
 app.listen(3333, () => { console.log('=> SERVER START') })
